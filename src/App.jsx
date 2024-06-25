ba46893f3b00c3c9111d255e3df3d9946443155b
HadiRachmat <hadirahmat712@gmail.com>
2024-06-25 12:10:17 +0700

    Hadi Rachmat Resume

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className=' text-neutral-950 font-bold text-center text-2xl'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
