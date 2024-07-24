1d925a8f1d35099b24f538b7e0feb119c39d6df4
HadiRachmat <hadirahmat712@gmail.com>
2024-07-25 05:09:03 +0700

    update profile

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -25,13 +25,10 @@ export default function About() {
           <div className="my-auto text-center sm:text-left">
             <h3 className="text-3xl font-serif"> Hadi Rachmat S </h3>
             <p className="py-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-              animi magni culpa eveniet laborum excepturi sit dignissimos fuga
-              reiciendis atque aliquid dolorem obcaecati cum doloremque sunt
-              enim molestiae, nobis vero? Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Blanditiis sapiente exercitationem, eius sint id
-              possimus. Et voluptatum eaque debitis at facilis cumque, excepturi
-              ipsum ratione, voluptatem eveniet ab omnis illo.
+              Holla.. My name is Hadi Rachmat Supindar, I was Born on july, 8, 2001 at sukabumi city
+              Now i live on Sukabumi city, West java Of Indonesia. My Current activities are as Student 
+              At Sukabumi Muhammadiyah University, My other job is as a freelance worker in the field of informatics
+              and then i as an active student majoring in informatics engineering
             </p>
             <button type="button" className="bg-[#F2613F] p-3 px-8 rounded-md hover:bg-[#49243E] hover:text-white hover:duration-500 active:animate-bounce active:delay-1000">
               My Resume
